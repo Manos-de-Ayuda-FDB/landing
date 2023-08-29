41634c5e072576cf2cc13e315531776b7ba06f1c
David Josue Rodríguez Chávez <davidjchavez98@gmail.com>
2023-08-28 19:51:04 -0600

    feat: Primer propuesta de landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import heroImg from '$lib/assets/hero-img.png';
+</script>
+
+<section class="hero min-h-[75vh]">
+    <div class="hero-content flex-col lg:flex-row-reverse">
+        <img src="{heroImg}"
+             alt="heroImg"
+             class="max-w-sm rounded-lg shadow-2xl"/>
+        <div>
+            <h1 class="text-6xl font-medium font-alice">
+                Fundación Manos de Ayuda FDB A.C.
+            </h1>
+            <p class="font-alice py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button class="btn btn-accent">Quiero ayudar</button>
+        </div>
+    </div>
+</section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-  disyui: {
-    themes: [
-      {
-        manosdeayuda: {
-          "primary": "#2A80FF",
-          "secondary": "#5233FF",
-          "accent": "#00DAAF",
-          "neutral": "#1D202F",
-          "base-100": "#EFEDF7",
-          "info": "#3F5DE4",
-          "success": "#29A397",
-          "warning": "#FBCC32",
-          "error": "#DD3145",
-        },
-      }
-    ]
-  }
+    content: ['./src/**/*.{html,js,svelte,ts}'],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                custom: {
+                    "primary": "#2A80FF",
+                    "secondary": "#5233FF",
+                    "accent": "#00DAAF",
+                    "neutral": "#1D202F",
+                    "base-100": "#EFEDF7",
+                    "info": "#3F5DE4",
+                    "success": "#29A397",
+                    "warning": "#FBCC32",
+                    "error": "#DD3145",
+                },
+            }
+        ]
+    }
 }
 
